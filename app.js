7282fbbd14560c74dae1f6b7a4e7e87574c93726
Avijit Biswas <avijitb440@gmail.com>
2019-10-06 09:48:42 +0530

    Completion of project

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+let items = ["buy food", "cook food", "eat food"];
+let workItems = [];
 app.get("/", (req, res) => {
   let today = new Date();
   let options = {
@@ -13,9 +14,22 @@ app.get("/", (req, res) => {
   };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day });
+  res.render("list", { listTitle: day, newListItems: items });
 });
-app.post("/", (req, res) => {});
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.post("/", (req, res) => {
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "work list", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
